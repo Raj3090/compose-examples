c504461e13cb281f1fd6177ac3819d587b4d04d7
Raj3090 <rajkumaryadav3009@gmail.com>
2021-09-01 21:43:57 +0530

    add checkbox

--- app/src/main/java/com/example/composelist/MainActivity.kt
+++ app/src/main/java/com/example/composelist/MainActivity.kt
@@ -6,17 +6,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -83,8 +81,22 @@ fun MovieRow(movie: Movie) {
                     .clip(CircleShape)
             )
             Text(text = movie.name,color = Color.White,textAlign = TextAlign.Center)
+            Spacer(modifier = Modifier.width(20.dp))
+            ColouredCheckbox()
         }
     }
+}
+
+@Composable
+fun ColouredCheckbox() {
+    val isChecked = remember { mutableStateOf(false) }
+
+    Checkbox(
+        checked = isChecked.value,
+        onCheckedChange = { isChecked.value = it },
+        enabled = true,
+        colors = CheckboxDefaults.colors(Color.Blue)
+    )
 }
 
 
--- app/src/main/java/com/example/composelist/data/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/composelist/data/MovieRepositoryImpl.kt
@@ -6,7 +6,7 @@ class MovieRepositoryImpl :MovieRepository{
 
     override suspend fun fetchMoviesList(): List<Movie> {
 
-       val response=MovieClient.getApiClient().create(MovieService::class.java).getPopularMovies().execute()
+//       val response=MovieClient.getApiClient().create(MovieService::class.java).getPopularMovies().execute()
 
         return listOf(Movie("SpiderMan","https://picsum.photos/200"),
             Movie("Hulk","https://picsum.photos/200")
